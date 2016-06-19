5c48fdc21cd8e01a2110cd85e8021fb7ec3a3ead
Borderlinehyper <borderlinehyper@gmail.com>
2016-06-19 20:19:59 +0100

    Changed directories
    Hopefully this works, changed to be able to directly refernce where the audio file is.

--- scott_bot.rb
+++ scott_bot.rb
@@ -1,6 +1,6 @@
 class ScottBot
 
-  ::RBNACL_LIBSODIUM_GEM_LIB_PATH = 'C:\libsodium.dll'
+  ::RBNACL_LIBSODIUM_GEM_LIB_PATH = 'libsodium.dll'
   require 'discordrb'
 
   bot = Discordrb::Commands::CommandBot.new token: 'MTg0NDM3NTE4ODY1NzkzMDI0.CiUaUw.ONJh2Ue-WzuPT-w1rbyoYw4Wzr4', application_id: 184437272823726080, prefix: '!'
@@ -13,15 +13,15 @@ class ScottBot
   bot.bucket :voices_long, limit: 4, time_span: 60,  delay:15
   bot.bucket :voices_short, limit: 10, time_span: 60, delay: 7
 
-  sassyquotes = ['C:\DiscordSounds\Sassy1.mp3', 'C:\DiscordSounds\Sassy2.mp3', 'C:\DiscordSounds\Sassy3.mp3',
-                 'C:\DiscordSounds\Sassy4.mp3', 'C:\DiscordSounds\Sassy5.mp3', 'C:\DiscordSounds\Sassy6.mp3',
-                 'C:\DiscordSounds\Sassy7.mp3']
+  sassyquotes = ['audio-files\Sassy1.mp3', 'audio-files\Sassy2.mp3', 'audio-files\Sassy3.mp3',
+                 'audio-files\Sassy4.mp3', 'audio-files\Sassy5.mp3', 'audio-files\Sassy6.mp3',
+                 'audio-files\Sassy7.mp3']
 
-  clarencequotes = ['C:\DiscordSounds\Clarence1.mp3', 'C:\DiscordSounds\Clarence2.mp3', 'C:\DiscordSounds\Clarence3.mp3']
+  clarencequotes = ['audio-files\Clarence1.mp3', 'audio-files\Clarence2.mp3', 'audio-files\Clarence3.mp3']
 
-  partysongs = ['C:\DiscordSounds\PartyInTheUSA.mp3']
+  partysongs = ['audio-files\PartyInTheUSA.mp3']
 
-  fucktracks = ['C:\DiscordSounds\fuck.mp3', 'C:\DiscordSounds\fuckmytomatoes.mp3']
+  fucktracks = ['audio-files\fuck.mp3', 'audio-files\fuckmytomatoes.mp3']
 
   voice = bot.find_channel('General')
   details = voice.inspect
@@ -39,49 +39,49 @@ class ScottBot
     inspect = id.inspect
     puts inspect
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\no.mp3')
+    event.voice.play_file('audio-files\no.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:cunt, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\cunt.wav')
+    event.voice.play_file('audio-files\cunt.wav')
     event.voice.destroy
     nil
   end
 
   bot.command(:johncena, bucket: :voices_long, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\johncena.mp3')
+    event.voice.play_file('audio-files\johncena.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:ainsley, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\ainslyharriot.mp3')
+    event.voice.play_file('audio-files\ainslyharriot.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:jamesbond, bucket: :voices_long, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\JamesBond.mp3')
+    event.voice.play_file('audio-files\JamesBond.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:stormz, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\bodiedbystormz.mp3')
+    event.voice.play_file('audio-files\bodiedbystormz.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:foxflute, bucket: :voices_long, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\foxflute.mp3')
+    event.voice.play_file('audio-files\foxflute.mp3')
     event.voice.destroy
     nil
   end
@@ -112,7 +112,7 @@ class ScottBot
 
   bot.command(:pureskill, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('C:\DiscordSounds\opsxtactics.mp3')
+    event.voice.play_file('audio-files\opsxtactics.mp3')
     event.voice.destroy
     nil
   end
