84a281340c982335f1d7b376d03458bc0336ad12
Borderlinehyper <borderlinehyper@gmail.com>
2016-06-19 20:47:42 +0100

    Added linux file directories

--- scott_bot.rb
+++ scott_bot.rb
@@ -1,6 +1,6 @@
 class ScottBot
 
-  ::RBNACL_LIBSODIUM_GEM_LIB_PATH = 'libsodium.dll'
+  ::RBNACL_LIBSODIUM_GEM_LIB_PATH = '/var/www/scottBot/libsodium.dll'
   require 'discordrb'
 
   bot = Discordrb::Commands::CommandBot.new token: 'MTg0NDM3NTE4ODY1NzkzMDI0.CiUaUw.ONJh2Ue-WzuPT-w1rbyoYw4Wzr4', application_id: 184437272823726080, prefix: '!'
@@ -13,15 +13,15 @@ class ScottBot
   bot.bucket :voices_long, limit: 4, time_span: 60,  delay:15
   bot.bucket :voices_short, limit: 10, time_span: 60, delay: 7
 
-  sassyquotes = ['audio-files\Sassy1.mp3', 'audio-files\Sassy2.mp3', 'audio-files\Sassy3.mp3',
-                 'audio-files\Sassy4.mp3', 'audio-files\Sassy5.mp3', 'audio-files\Sassy6.mp3',
-                 'audio-files\Sassy7.mp3']
+  sassyquotes = ['/var/www/scottBot/Sassy1.mp3', '/var/www/scottBot/Sassy2.mp3', '/var/www/scottBot/Sassy3.mp3',
+                 '/var/www/scottBot/Sassy4.mp3', '/var/www/scottBot/Sassy5.mp3', '/var/www/scottBot/Sassy6.mp3',
+                 '/var/www/scottBot/Sassy7.mp3']
 
-  clarencequotes = ['audio-files\Clarence1.mp3', 'audio-files\Clarence2.mp3', 'audio-files\Clarence3.mp3']
+  clarencequotes = ['/var/www/scottBot/Clarence1.mp3', '/var/www/scottBot/Clarence2.mp3', '/var/www/scottBot/Clarence3.mp3']
 
-  partysongs = ['audio-files\PartyInTheUSA.mp3']
+  partysongs = ['/var/www/scottBot/PartyInTheUSA.mp3']
 
-  fucktracks = ['audio-files\fuck.mp3', 'audio-files\fuckmytomatoes.mp3']
+  fucktracks = ['/var/www/scottBot/fuck.mp3', '/var/www/scottBot/fuckmytomatoes.mp3']
 
   voice = bot.find_channel('General')
   details = voice.inspect
@@ -39,49 +39,49 @@ class ScottBot
     inspect = id.inspect
     puts inspect
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\no.mp3')
+    event.voice.play_file('/var/www/scottBot/no.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:cunt, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\cunt.wav')
+    event.voice.play_file('/var/www/scottBot/cunt.wav')
     event.voice.destroy
     nil
   end
 
   bot.command(:johncena, bucket: :voices_long, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\johncena.mp3')
+    event.voice.play_file('/var/www/scottBot/johncena.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:ainsley, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\ainslyharriot.mp3')
+    event.voice.play_file('/var/www/scottBot/ainslyharriot.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:jamesbond, bucket: :voices_long, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\JamesBond.mp3')
+    event.voice.play_file('/var/www/scottBot/JamesBond.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:stormz, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\bodiedbystormz.mp3')
+    event.voice.play_file('/var/www/scottBot/bodiedbystormz.mp3')
     event.voice.destroy
     nil
   end
 
   bot.command(:foxflute, bucket: :voices_long, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\foxflute.mp3')
+    event.voice.play_file('/var/www/scottBot/foxflute.mp3')
     event.voice.destroy
     nil
   end
@@ -112,7 +112,7 @@ class ScottBot
 
   bot.command(:pureskill, bucket: :voices_short, rate_limit_message:(rate_message)) do |event|
     bot.voice_connect(180763507594100737)
-    event.voice.play_file('audio-files\opsxtactics.mp3')
+    event.voice.play_file('/var/www/scottBot/opsxtactics.mp3')
     event.voice.destroy
     nil
   end
